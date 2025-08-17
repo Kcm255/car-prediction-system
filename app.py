ccae659aeb78b4c13e50d76bdf205b42b6f4318b
kcm@225 <krishmehta9845@gmail.com>
2025-08-17 10:53:31 +0530

    Updated app.py for deployment

--- app.py
+++ app.py
@@ -58,3 +58,10 @@ def predict():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+import os
+port = int(os.environ.get("PORT", 5000))
+
+if __name__ == "__main__":
+    app.run(debug=False, host='0.0.0.0', port=port)
+
